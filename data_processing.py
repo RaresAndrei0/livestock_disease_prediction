625bf63af5702aa8e689dec4e2cf2db02e31f965
RaresAndrei0 <info.rares17@gmail.com>
2025-05-06 09:49:06 +0300

    ...

--- data_processing.py
+++ data_processing.py
@@ -1,1 +1,9 @@
 import pandas as pd
+from sklearn.preprocessing import LabelEncoder, MultiLabelBinarizer
+
+def load_and_process_data():
+    df = pd.read_csv('livestock_disease.csv')
+    df['Symptoms'] = df['Symptoms'].str.lower().str.split(',')
+
+    mlb = MultiLabelBinarizer()
+    symptoms_encoded = pd.DataFrame(mlb.fit_transform(df['Symptoms']),columns=mlb.classes_)
